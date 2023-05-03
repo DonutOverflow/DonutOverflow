bdf15d3b2eba54d654cb36f742ddcfd981b0663d
vladyc1234 <73032808+vladyc1234@users.noreply.github.com>
2023-05-04 00:16:31 +0300

    Update ProjectApplicationTests.java

--- DonutOverflow/src/test/java/PAO/example/project/ProjectApplicationTests.java
+++ DonutOverflow/src/test/java/PAO/example/project/ProjectApplicationTests.java
@@ -6,16 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import java.time.LocalDate;
 
-@SpringBootTest
 class ProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-	LocalDate dob = LocalDate.of(2001, 7, 16);
-	Client client = new Client("vlad", "vlad", "vlad@gmail.com", dob);
-	@Test
-	void test_id(){
-		Assertions.assertEquals(1, client.getId());
-		}
 }
